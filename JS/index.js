739c6e4638d04b6076155b32ec9d903867692517
ophgdy <opheliedelamole@gmail.com>
2023-06-27 11:33:46 +0200

    Bouton activer dark/clear

--- JS/index.js
+++ JS/index.js
@@ -3,26 +3,43 @@
  * @returns {undefined} aucun retour
  */
 function init(params) {
-    var currentDate=new Date();
-    console.log(currentDate.toISOString());
-    var footer=document.getElementsByTagName('footer')[0];
-    footer.innerHTML=(new Date()).toISOString();
-    footer.style.color='white';
-    footer.style.backgroundColor='rgba(50,50,50,0.2)';
-    footer.style.fontStyle='italic';
-    footer.style.textDecoration='Underline';
+  document
+    .querySelector("#flexSwitchCheckDefault")
+    .addEventListener("change", function (evt) {
+      changetheme(evt.target.checked);
+    });
+  var currentDate = new Date();
+  console.log(currentDate.toISOString());
+  var footer = document.getElementsByTagName("footer")[0];
+  footer.innerHTML = new Date().toISOString();
+  footer.style.color = "white";
+  footer.style.backgroundColor = "rgba(50,50,50,0.2)";
+  footer.style.fontStyle = "italic";
+  footer.style.textDecoration = "Underline";
 }
 /**
  * changement d'état du theme
  * @param {boolean} isDark etat du choix de theme dark/clear
  */
 function changetheme(isDark) {
-    if(isDark){
-        document.body.className='dark'; }
-        else{document.body.className='';}
-    }
-document.addEventListener('DOMContentLoaded',function(evt){
-    console.log(evt);
-    init()
-})
-
+  var nav = document.getElementsByTagName("nav")[0];
+  var slider = document.getElementById("flexSwitchCheckDefault");
+  var lbl = document.querySelector("#theme label");
+  if (isDark) {
+    document.body.className = "dark";
+    nav.classList.replace("navbar-light", "navbar-dark");
+    nav.classList.replace("bg-light", "bg-dark");
+    slider.checked = true;
+    lbl.innerHTML = "Dark";
+  } else {
+    document.body.className = "";
+    nav.classList.replace("navbar-dark", "navbar-light");
+    nav.classList.replace("bg-dark", "bg-light");
+    slider.checked = false;
+    lbl.innerHTML = "Clear";
+  }
+}
+document.addEventListener("DOMContentLoaded", function (evt) {
+  console.log(evt);
+  init();
+});
