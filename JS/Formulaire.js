cb1fe0961ed9a791f6187a2393b9903e04fbf696
ophgdy <opheliedelamole@gmail.com>
2023-06-27 16:21:19 +0200

    le cracage

--- JS/Formulaire.js
+++ JS/Formulaire.js
@@ -1,32 +1,65 @@
 var currentMeme = new Meme();
 //console.log(currentMeme);
 function InitMemeEditor() {
-    var form=document.forms['meme-form'];
+  var form = document.forms["meme-form"];
 
-    form['titre'].addEventListener('input', function(evt){
-        currentMeme.titre=evt.target.value
-    })
-    
-    form['text'].addEventListener('input', function(evt){
-        currentMeme.text=evt.target.value
-    })
-    
-    form['x'].addEventListener('input', function(evt){
-        currentMeme.x=Number (evt.target.value);
-    })
-    
-    form['y'].addEventListener('input', function(evt){
-        currentMeme.y=Number (evt.target.value);
-    })
-    // var form=document.forms['meme-form'];
-    // form['color'].addEventListener('input', function(evt){
-    //     currentMeme.color=evt.target.value
-    // })
-    // var form=document.forms['meme-form'];
-    // form['fontSize'].addEventListener('input', function(evt){
-    //     cursrentMeme.fonySize=evt.target.value
-    // })
-    
-    form['imageId'].addEventListener('change', function(evt){
-        cursrentMeme.ImageId=evt.target.value
-    })}
+  form["titre"].addEventListener("input", function (evt) {
+    currentMeme.titre = evt.target.value;
+    renderMeme();
+  });
+
+  form["text"].addEventListener("input", function (evt) {
+    currentMeme.text = evt.target.value;
+    renderMeme();
+  });
+
+  form["x"].addEventListener("input", function (evt) {
+    currentMeme.x = Number(evt.target.value);
+    renderMeme();
+  });
+
+  form["y"].addEventListener("input", function (evt) {
+    currentMeme.y = Number(evt.target.value);
+    renderMeme();
+  });
+
+  form["color"].addEventListener("input", function (evt) {
+    currentMeme.color = evt.target.value;
+    renderMeme();
+  });
+  // var form=document.forms['meme-form'];
+  // form['fontSize'].addEventListener('input', function(evt){
+  //     cursrentMeme.fonySize=evt.target.value
+  // })
+
+  form["imageId"].addEventListener("change", function (evt) {
+    currentMeme.ImageId = evt.target.value;
+    renderMeme();
+  });
+}
+function renderMeme(meme) {
+  if (undefined === meme) {
+    meme = currentMeme;
+  }
+  var svg = document.querySelector("#editor-viewer svg");
+  var texteElement = svg.querySelector("text");
+  texteElement.innerHTML=meme.text;
+  texteElement.style.fill = meme.color;
+  texteElement.style.textDecoration = meme.underline ? "underline" : "none";
+  texteElement.setAttribute('x',meme.x);
+  texteElement.setAttribute('y',meme.y);
+}
+function loadSelectImage(image) {
+  var select = document.forms["meme-form"]['imageId'];
+  var optBase=document.createElement('option');
+  optBase.value="etry";
+  optBase.innerHTML='text visuel';
+  select.appendChild(optBase)
+  image.forEach(function(img){
+    var opt=optBase.cloneNode(true);
+    opt.value=img.id;
+    opt.innerHTML=img.titre;
+    select.appendChild(opt);
+       
+  });
+}
--- JS/Meme.js
+++ JS/Meme.js
@@ -4,7 +4,7 @@ function Meme() {
     this.x=0;
     this.y=20;
     this.imageId=-1;
-    this.fontSize=20;
+    this.fontSize=40;
     this.fontWeigt='500';
     this.underline=false;
     this.italic=false;
