d53e4bb12ae20e9482c484bfa26b234e8e20ec25
ophgdy <opheliedelamole@gmail.com>
2023-06-27 09:47:09 +0200

    Style de balise par JS

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,16 @@
  */
 function init(params) {
     var currentDate=new Date();
-    console.log(currentDate.toISOString())
+    console.log(currentDate.toISOString());
+    var footer=document.getElementsByTagName('footer')[0];
+    footer.innerHTML=(new Date()).toISOString();
+    footer.style.color='white';
+    footer.style.backgroundColor='rgba(50,50,50,0.2)';
+    footer.style.fontStyle='italic';
+    footer.style.textDecoration='Underline';
 }
-init()
+document.addEventListener('DOMContentLoaded',function(evt){
+    console.log(evt);
+    init()
+})
+
