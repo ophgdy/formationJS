98d22cf8d5d99e39e8e151a1834a7862103314a7
ophgdy <opheliedelamole@gmail.com>
2023-06-28 11:14:05 +0200

    passe objet+ ddef des routes

--- JS/routeur.js
+++ JS/routeur.js
@@ -1,13 +1,37 @@
 /**
  * variable de config des routes
  */
-const routeConfig={
-    route:[]
+const routeConfig = {
+  route: [
+    {
+      path: "/thumbail",
+      initialisation: undefined,
+      templateUrl: "/view/thumbnail.html",
+    },
+    {
+      path: "/",
+      initialisation: undefined,
+      templateUrl: "/view/home.html",
+    },
+    {
+      path: "/break",
+      initialisation: undefined,
+      templateUrl: "/view/templateQuiExistePasSurLeServeur.html",
+    },
+  ],
+};
+
+class Router {
+  handleRoute() {
+    const pathName = location.pathname;
+    console.log(pathName);
+  }
+  /**
+   * navigate to
+   * @param {string} patName chmin commencant par /
+   */
+  changeRoute(pathName) {}
 }
-/**
- * fonction de maintient de route
- */
-export const handleRoute=() => {
-    const pathName=location.pathname;
-    console.log(pathName)
-}
+const router = new this.router();
+router.handleRoute();
+router.changeRoute();
