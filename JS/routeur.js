017128c273a720f5424c76ee4fa73e8673442c6c
ophgdy <opheliedelamole@gmail.com>
2023-06-28 10:58:53 +0200

    Je sais pas ce qio c'est passer

---
+++ JS/routeur.js
@@ -0,0 +1,13 @@
+/**
+ * variable de config des routes
+ */
+const routeConfig={
+    route:[]
+}
+/**
+ * fonction de maintient de route
+ */
+export const handleRoute=() => {
+    const pathName=location.pathname;
+    console.log(pathName)
+}
