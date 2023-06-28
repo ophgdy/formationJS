5f3586510d1669ec130a2948a270a6df8cc1cbfe
ophgdy <opheliedelamole@gmail.com>
2023-06-28 15:36:32 +0200

    Meme.js

---
+++ JS/metier/Meme.js
@@ -0,0 +1,12 @@
+export class Meme {
+  titre = "";
+  text = "";
+  x = 0;
+  y = 20;
+  imageId = -1;
+  fontSize = 20;
+  fontSize = 40;
+  fontWeigt = "500";
+  underline = false;
+  italic = false;
+}
